89f2fda24c46c85315e508e38d6dff16d3c9b9b4
phoenix-tckls <109603706+phoenix-tckls@users.noreply.github.com>
2024-03-04 22:35:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.graph_objects as go
 from datetime import datetime
+import pandas as pd
+import numpy as np
 
 # Initialize connection.
 conn = st.connection("snowflake")
@@ -9,20 +11,78 @@ df_ff = conn.query(
     """
     select * from forexfactory order by datetime desc;
     """, 
-    ttl=600)
+    ttl=600
+)
 
 query = st.text_input("Filter dataframe")
+
+st.dataframe(df_ff)
 
 if query:
     mask = df_ff.map(lambda x: query.lower() in str(x).lower()).any(axis=1)
     df_ff = df_ff[mask]
 
-st.data_editor(df_ff)
+    dates_result = df_ff['DATETIME'].astype(str).tolist()
+    minute_window_input = st.text_input("Adjust window for minutes ahead (default = 60min)")
+
+    def get_movement_with_timewindow(dates_result, minute_window_input):
+        all_rows = []
+        for d in dates_result:
+            df_high_low = conn.query(
+                f"""
+                    select 
+                        max(high) as max_high_in_{minute_window_input}_min,
+                        min(low) as min_low_in_{minute_window_input}_min
+                    from (
+                        select * from gold.processed.ohlc 
+                        where datetime > '{d}'
+                        order by datetime 
+                        limit {minute_window_input}
+                    )
+                """, 
+                ttl=600
+            )
+
+            df_open = conn.query(
+                f"""
+                    select 
+                        datetime, 
+                        open as open_price_at_timestamp
+                    from gold.processed.ohlc 
+                    where datetime = '{d}'
+                """, 
+                ttl=600
+            )
+
+            data = pd.concat([df_open,df_high_low], axis=1).iloc[0].tolist()
+            all_rows.append(data)
+
+        df = pd.DataFrame(all_rows, columns=['DATETIME','OPEN_PRICE_AT_TIMESTAMP',f'MAX_HIGH_IN_{minute_window_input}_MIN',f'MIN_LOW_IN_{minute_window_input}_MIN'])
+        df["open-high"] = abs(df['OPEN_PRICE_AT_TIMESTAMP'] - df[f'MAX_HIGH_IN_{minute_window_input}_MIN'])
+        df["open-low"] = abs(df['OPEN_PRICE_AT_TIMESTAMP'] - df[f'MIN_LOW_IN_{minute_window_input}_MIN'])
+        df["LARGEST_DIFF"] = np.where(df["open-high"] > df["open-low"], df["open-high"], df["open-low"])
+        df["LARGEST_DIFF_CLASS"] = np.where(df["open-high"] > df["open-low"], "High", "Low")
+        df = df.drop(["open-high","open-low"], axis=1)
+        df = df[df['DATETIME'].notna()]
+
+        return df
+
+    try:
+        df = df_ff[["DATETIME","DESCRIPTION","ACTUAL","FORECAST","PREVIOUS"]].merge(get_movement_with_timewindow(dates_result, minute_window_input))
+    except:
+        df = df_ff[["DATETIME","DESCRIPTION","ACTUAL","FORECAST","PREVIOUS"]].merge(get_movement_with_timewindow(dates_result, 60))
+    
+    st.dataframe(df)
+
+
 
 dates_for_ohlc_1 = st.text_input("Input start date range for OHLC [YYYY-MM-DD]")
 dates_for_ohlc_2 = st.text_input("Input end date range for OHLC [YYYY-MM-DD]")
 
-df_xau_usd = conn.query(f"select * from ohlc where date(datetime) >= '{dates_for_ohlc_1}' and date(datetime) <= '{dates_for_ohlc_2}';", ttl=600)
+try:
+    df_xau_usd = conn.query(f"select * from ohlc where date(datetime) >= '{dates_for_ohlc_1}' and date(datetime) <= '{dates_for_ohlc_2}'", ttl=600)
+except:
+    df_xau_usd = conn.query(f"select * from ohlc order by datetime desc limit 50", ttl=600)
 
 fig = go.Figure()
 fig.add_trace(
@@ -35,22 +95,3 @@ fig.add_trace(
     )
 )
 st.plotly_chart(fig)
-
-
-timestamp = st.text_input("Event timestamp: timestamp open vs. timestamp low / high price within + 1h")
-open_original = conn.query(f"select open from ohlc where datetime = to_timestamp('{timestamp}');", ttl=600)["OPEN"][0]
-highest_within_plus_1h = conn.query(f"SELECT datetime as minutes_after, max(high) as highest from ohlc where datetime > to_timestamp('{timestamp}')and datetime <= dateadd(hour,1,to_timestamp('{timestamp}')) group by 1 order by 2 desc;", ttl=600).iloc[0]
-lowest_within_plus_1h = conn.query(f"SELECT datetime as minutes_after, min(low) as lowest from ohlc where datetime > to_timestamp('{timestamp}')and datetime <= dateadd(hour,1,to_timestamp('{timestamp}')) group by 1 order by 2;", ttl=600).iloc[0]
-highest_price = highest_within_plus_1h['HIGHEST']
-highest_minutes_after = (datetime.strptime(str(highest_within_plus_1h['MINUTES_AFTER']), '%Y-%m-%d %H:%M:%S') - datetime.strptime(str(timestamp), '%Y-%m-%d %H:%M:%S')).total_seconds() / 60.0
-lowest_price = lowest_within_plus_1h['LOWEST']
-lowest_minutes_after = (datetime.strptime(str(lowest_within_plus_1h['MINUTES_AFTER']), '%Y-%m-%d %H:%M:%S') - datetime.strptime(str(timestamp), '%Y-%m-%d %H:%M:%S')).total_seconds() / 60.0
-st.write(f"Open amount (timestamp of event): {open_original}")
-st.write(f"High amount (timestamp of event + {highest_minutes_after} minutes): {highest_price}")
-st.write(f"Low amount (timestamp of event + {lowest_minutes_after} minutes): {lowest_price}")
-if abs(open_original-highest_price) > abs(open_original-lowest_price):
-    st.write(f"Biggest difference: {abs(round(open_original-highest_price, 3))} (high)")
-elif abs(open_original-lowest_price) > abs(open_original-highest_price):
-    st.write(f"Biggest difference: {abs(round(open_original-lowest_price, 3))} (low)")
-else:
-    st.write(f"Biggest difference: {abs(round(open_original-highest_price, 3))}")
